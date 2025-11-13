b3a471e1e6d425466297b4f71bf60f80d430dc90
david-dya4yk <daviddya4yk@mail.ru>
2025-11-14 01:10:34 +0200

    fix: delete comments and logs

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,9 +1,9 @@
 module.exports = {
-  semi: true, // ставить крапки з комою
-  singleQuote: true, // одинарні лапки
-  trailingComma: 'es5', // кома після об'єктів/масивів
-  tabWidth: 2, // 2 пробіли на відступ
-  printWidth: 80, // перенос рядків після 80 символів
-  bracketSpacing: true, // { foo: bar } замість {foo:bar}
-  arrowParens: 'avoid', // (x) => ... → x => ...
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  printWidth: 80,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
 };
